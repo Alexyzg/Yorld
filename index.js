26aa2d434def321bc766d71c998073d23d418315
Aliaksandr Prakapovich <aliaksandr_prakapovich@epam.com>
2021-07-15 23:58:49 +0300

    Added mapbox for ios

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
+import { AppRegistry } from 'react-native';
+import { App } from './App';
+import { name as appName } from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import MapboxGL from "@react-native-mapbox-gl/maps";
+
+MapboxGL.setAccessToken("pk.eyJ1IjoiYWxpdHNrZXZpY2giLCJhIjoiY2p5bWtwYmgwMGluZDNpbXRwMWk2eG51NyJ9.PHQubjbq-xlcJJGeBT4yNw");
 
 AppRegistry.registerComponent(appName, () => App);
