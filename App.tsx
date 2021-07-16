57bb15e58ce95c463aac8a66e23608925d98fb46
Aliaksandr Prakapovich <aliaksandr_prakapovich@epam.com>
2021-07-16 21:34:09 +0300

    Added 3d layer and camera zoom on vilinius

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { StyleProp, StyleSheet } from 'react-native';
 
-import MapboxGL from "@react-native-mapbox-gl/maps";
+import MapboxGL, { FillExtrusionLayerStyle } from "@react-native-mapbox-gl/maps";
 
+export const MapboxStyles = 'mapbox://styles/alyonayanuchek/ck9pti76v3cic1ilchpjj6obq';
 const styles = StyleSheet.create({
   map: {
     width: '100%',
@@ -10,6 +11,42 @@ const styles = StyleSheet.create({
   }
 });
 
+const centerVilnius = [25.279652, 54.687157]
 export const App = () => (
-  <MapboxGL.MapView style={styles.map} />
+  <MapboxGL.MapView
+    style={styles.map}
+    styleURL={MapboxStyles}
+    surfaceView
+    compassEnabled={false}
+  >
+    <MapboxGL.Camera pitch={60} zoomLevel={14} centerCoordinate={centerVilnius} />
+    <MapboxGL.FillExtrusionLayer
+      id='building3d'
+      sourceLayerID='building'
+      style={fillExtrusionLayerStyle}
+    />
+  </MapboxGL.MapView>
 );
+
+const fillExtrusionLayerStyle = {
+  fillExtrusionHeight: [
+    'interpolate',
+    ['linear'],
+    ['zoom'],
+    15,
+    0,
+    15.05,
+    ['get', 'height']
+  ],
+  fillExtrusionBase: [
+    'interpolate',
+    ['linear'],
+    ['zoom'],
+    15,
+    0,
+    15.05,
+    ['get', 'min_height']
+  ],
+  fillExtrusionColor: '#fff',
+  fillExtrusionOpacity: 0.6
+} as StyleProp<FillExtrusionLayerStyle>;
