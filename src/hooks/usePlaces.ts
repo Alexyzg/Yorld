f867290e0851363e50a5ab5b949df7b2b60f6d0f
Aliaksandr Prakapovich <aliaksandr_prakapovich@epam.com>
2021-10-02 18:29:20 +0300

    slice "place" to 20

--- src/hooks/usePlaces.ts
+++ src/hooks/usePlaces.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Place } from '../types';
 
-const data = require('../../data.json');
+const data = require('../../data.json').slice(0, 20);
 
 export const usePlaces = () => {
   const [places, setPlaces] = useState<Place[] | undefined>();
